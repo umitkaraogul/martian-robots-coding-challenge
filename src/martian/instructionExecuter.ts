2f0088ef745809828113929916b3c67cf10ad889
Umit <ukaraogul@gmail.com>
2023-03-01 22:26:57 +0000

    feat: implement instruction parser

--- src/martian/instructionExecuter.ts
+++ src/martian/instructionExecuter.ts
@@ -2,6 +2,12 @@ import { MAXIMUM_COORDINATE, MAXIMUM_INSTRUCTION } from './config';
 import Robot from './Robot';
 import { Coordinate, Grid, Instruction, Orientation } from './types';
 
+type RobotDetails = {
+  coordinate: Coordinate;
+  orientation: Orientation;
+  instructions: Instruction[];
+};
+
 const executeInstruction = (instruction: string): string[] => {
   //TODO: execute instruction and return final position of the robot
   const result: string[] = [];
@@ -17,17 +23,19 @@ const executeInstruction = (instruction: string): string[] => {
   return result;
 };
 
-//TODO:
 const parseInstruction = (instruction: string) => {
   if (instruction.length > MAXIMUM_INSTRUCTION) {
     throw new Error(
       `All instruction strings will be less than ${MAXIMUM_INSTRUCTION} characters in length.`
     );
   }
-  //TODO:  Dummy data; get from parsed instruction
+
+  //Get Grid info
+  const commandLines = instruction.split('\n').filter(Boolean);
+  const [x, y] = commandLines[0].split(' ');
   const grid: Grid = {
-    width: 5,
-    height: 3
+    width: parseInt(x),
+    height: parseInt(y)
   };
 
   if ([grid.width, grid.height].some((value) => value > MAXIMUM_COORDINATE)) {
@@ -35,29 +43,35 @@ const parseInstruction = (instruction: string) => {
       `The maximum value for any coordinate is ${MAXIMUM_COORDINATE}`
     );
   }
-  //TODO: Dummy data; get from parsed instruction
-  const robotDetails: {
-    coordinate: Coordinate;
-    orientation: Orientation;
-    instructions: Instruction[];
-  }[] = [
-    {
-      coordinate: {
-        x: 1,
-        y: 1
-      },
-      orientation: 'E',
-      instructions: 'RFRFRFRF'.split('') as Instruction[]
-    },
-    {
-      coordinate: {
-        x: 3,
-        y: 2
-      },
-      orientation: 'N',
-      instructions: 'FRRFLLFFRRFLL'.split('') as Instruction[]
+
+  //Get Robot Details
+  const robotDetails: RobotDetails[] = [];
+  for (let i = 1; i < commandLines.length; i += 2) {
+    const [x, y, direction] = commandLines[i].split(' ');
+    const coordinate: Coordinate = {
+      x: parseInt(x),
+      y: parseInt(y)
+    };
+
+    //TODO: It can be a util method.
+    if (
+      [coordinate.x, coordinate.y].some((value) => value > MAXIMUM_COORDINATE)
+    ) {
+      throw new Error(
+        `The maximum value for any coordinate is ${MAXIMUM_COORDINATE}`
+      );
     }
-  ];
+
+    const orientation = direction as Orientation;
+    const instructions = commandLines[i + 1].split('') as Instruction[];
+
+    robotDetails.push({
+      coordinate,
+      orientation,
+      instructions
+    });
+  }
+
   return { grid, robotDetails };
 };
 
