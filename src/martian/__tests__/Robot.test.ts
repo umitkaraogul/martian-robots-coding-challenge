92f8e6a2c646a8f28c8c2acdded06c29507d920a
Umit <ukaraogul@gmail.com>
2023-03-01 22:29:58 +0000

    test: ane new test cases to Robot test

--- src/martian/__tests__/Robot.test.ts
+++ src/martian/__tests__/Robot.test.ts
@@ -35,7 +35,6 @@ describe('Robot', () => {
     expect(robot.coordinate.y).toEqual(1);
     expect(robot.orientation).toEqual('W');
   });
-
   it('should turn right correctly', () => {
     const robot = new Robot(
       {
@@ -66,7 +65,7 @@ describe('Robot', () => {
     expect(robot.orientation).toEqual('N');
   });
   describe('should lose when falls off the edge of the grid', () => {
-    it('should print "3 3 N LOST"  when given FRRFLLFFRRFLL', () => {
+    it('should print "3 3 N LOST" when given "3 2 N" and FRRFLLFFRRFLL', () => {
       const robot = new Robot(
         {
           x: 3,
@@ -80,6 +79,51 @@ describe('Robot', () => {
 
       expect(robot.isLost).toBeTruthy();
       expect(robot.position).toBe('3 3 N LOST');
+    });
+    it('should print "3 3 N LOST" when given "0 3 W" and LLFFFLFLFL', () => {
+      const robot = new Robot(
+        {
+          x: 0,
+          y: 3
+        },
+        'W',
+        grid
+      );
+      const instructions = 'LLFFFLFLFL'.split('') as Instruction[];
+      robot.move(instructions);
+
+      expect(robot.isLost).toBeTruthy();
+      expect(robot.position).toBe('3 3 N LOST');
+    });
+  });
+  describe('should move correctly with multiple instruction', () => {
+    it('should move 1 1 E  when given RFRFRFRF', () => {
+      const robot = new Robot(
+        {
+          x: 1,
+          y: 1
+        },
+        'E',
+        grid
+      );
+      const instructions = 'RFRFRFRF'.split('') as Instruction[];
+      robot.move(instructions);
+
+      expect(robot.position).toBe('1 1 E');
+    });
+    it('should move 2 2 N when given FRRFLLFRL', () => {
+      const robot = new Robot(
+        {
+          x: 2,
+          y: 2
+        },
+        'N',
+        grid
+      );
+      const instructions = 'FRRFLLFRL'.split('') as Instruction[];
+      robot.move(instructions);
+
+      expect(robot.position).toBe('2 3 N');
     });
   });
 });
